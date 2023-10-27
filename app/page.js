5e3a7633abafc81f224baec3f5056e6bdce0d1a7
Carlos Ruiz <carlos.rz.fdz@gmail.com>
2023-10-26 19:13:29 -0600

    Small Changes

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className={"place-content-center border-16 border-transparent h-screen"}>
       <div className={"bg-purple-800 rounded-lg h-3/6 border-2 border-black flex-auto hover:h-5/6 ease-in duration-300"}>
-        <p className={"text-center"}> Hello world! </p>
+        <p className={"text-center"}> Hello world! G&C </p>
       </div>
       <div className={"bg-teal-500 rounded-lg h-3/6 border-2 border-black flex-auto"} >
-          <p className={"text-center"}>part 2</p>
+          <p className={"text-center"}>Paper rings</p>
       </div>
     </main>
   )
