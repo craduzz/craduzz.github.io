0cd0963fb0e76b48f0dae7cc6c4673d02eddd9d4
Carlos Ruiz <carlos.rz.fdz@gmail.com>
2023-10-26 18:38:40 -0600

    just playing around

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,14 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    borderWidth:{
+      DEFAULT: '1px',
+      '0':'0',
+      '2':'2px',
+      '4':'4px',
+      '16':'16px',
+      '20':'20px'
+    }
   },
   plugins: [],
 }
